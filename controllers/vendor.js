eb8cd0da67c1c0a53bfe0c4586e46f462cb2d37a
namanja <naman.na.jain@oracle.com>
2023-01-20 23:05:13 +0530

    Location Filter

--- controllers/vendor.js
+++ controllers/vendor.js
@@ -331,11 +331,14 @@ const handleSub = async (req, res, sub) => {
 };
 
 exports.searchfilters = async (req, res) => {
-  const { query, price, category, stars, sub } = req.body;
+  const { query, price, category, stars, sub, areas } = req.body;
   var qry = {};
-  console.log(req.body);
+  console.log(req.body, "abc");
   if (query) {
     qry.$text = { $search: query };
+  }
+  if (areas && areas.length > 0) {
+    qry.areasCovered = { $in: areas };
   }
   if (price) {
     qry.price = {
