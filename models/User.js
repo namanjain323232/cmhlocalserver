f7e4b5383c4a31e072cca300f09c41d848ae3b8f
namanja <naman.na.jain@oracle.com>
2023-02-13 23:34:06 +0530

    mails

--- models/User.js
+++ models/User.js
@@ -1,39 +1,41 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
-const userSchema = new schema({
+const userSchema = new schema(
+  {
     email: {
-        type:String,
-        unique: true,
-        index:true
+      type: String,
+      unique: true,
+      index: true,
     },
     role: {
-        type: String,
-        default: "subscriber"
+      type: String,
+      default: "subscriber",
     },
     password: {
-        type:String        
+      type: String,
     },
     name: {
-        type: String
-    } ,
+      type: String,
+    },
     googleId: String,
     facebookId: String,
     url: String,
     picture: {
-        type: String
+      type: String,
     },
     cart: {
       type: Array,
-      default: []
+      default: [],
     },
     address: String,
-    stripe_account_id:"",
+    phone: String,
+    stripe_account_id: "",
     stripe_seller: {},
-    stripeSession: {}    
- },
- {timestamps: true}
- );
+    stripeSession: {},
+  },
+  { timestamps: true }
+);
 
- // two arguments mean we are trying to load something into the model
- mongoose.exports = User = mongoose.model("User",userSchema);
+// two arguments mean we are trying to load something into the model
+mongoose.exports = User = mongoose.model("User", userSchema);
